f9de8fec6a3829e578a442aefc3f53ff903ddf1e
Emmanuel Campos <dev.ejcampos@gmail.com>
2021-01-20 14:17:23 +0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { add } from "./services/math"
+
+function init() {
+    const sum = add(1, 1)
+    console.log(sum)
+}
+
+init()
---
+++ services/math.test.ts
@@ -0,0 +1,7 @@
+import { add } from "./math"
+
+describe('add', () => {
+    it('should add 2 numbers', () => {
+        expect(add(1, 1)).toBe(2)
+    })
+})
